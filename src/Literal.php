39772a735a40f7c95ccc4e8b0c257af668270995
Bishop Bettini <bishop.bettini@gmail.com>
2015-05-29 11:48:40 -0400

    Refactor construction to an abstract pattern

--- src/Literal.php
+++ src/Literal.php
@@ -1,26 +1,11 @@
 <?php
 namespace BishopB\Pattern;
 
-use \BishopB\Pattern\Exception\InvalidArgument;
+use \BishopB\Pattern\Pattern;
 
 /**
  * Represents a literal pattern, one that has no meta-characters.
  */
-class Literal
+class Literal extends Pattern
 {
-    /**
-     * Create a Literal pattern.
-     * @param string $pattern The literal pattern to use.
-     * @throws \BishopB\Pattern\Exception\InvalidArgument if not given a string
-     */
-    public function __construct($pattern)
-    {
-        if (is_string($string)) {
-        } else {
-            throw new InvalidArgument(sprintf(
-                'Pattern given as type %s, expecting string',
-                gettype($pattern)
-            ));
-        }
-    }
 }
---
+++ src/Pattern.php
@@ -0,0 +1,27 @@
+<?php
+namespace BishopB\Pattern;
+
+use \BishopB\Pattern\Exception\InvalidArgument;
+
+/**
+ * Represents a generic pattern and acts as a base class for all patterns in
+ * this library.
+ */
+abstract class Pattern
+{
+    /**
+     * Create a pattern.
+     * @param string $pattern The pattern to use.
+     * @throws \BishopB\Pattern\Exception\InvalidArgument if not given a string
+     */
+    public function __construct($pattern)
+    {
+        if (is_string($string)) {
+        } else {
+            throw new InvalidArgument(sprintf(
+                'Pattern given as type %s, expecting string',
+                gettype($pattern)
+            ));
+        }
+    }
+}
